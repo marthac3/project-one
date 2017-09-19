222310577c7158ca07e6f33ae25a9a4b8badb6fd
Martha Conway <marthaconway93@gmail.com>
2017-09-19 12:08:46 +0100

    square number calculations now in loops

--- js/main.js
+++ js/main.js
@@ -8,24 +8,30 @@ $(function() {
 	function run() {
 		createGrid();
 		clickSquare();
-		console.log();
+		//console.log();
 	}
 
-	// create 3x3 grid
-		// create grid based on difficulty selected
-
+		// creates a square grid for a given size
 		function createGrid() {
 			var sideLength = 600/gridWidth;
+
+			// creates arrays of all squares on the edges of the grid
+			topArray = sideArray("top");
+			bottomArray = sideArray("bottom");
+			leftArray = sideArray("left");
+			rightArray = sideArray("right");
 			
+			// creates squares
 			for (var i = 0; i < gridSize; i++) {
 				$("ul").append($("<li id='"+i+"'></li>"));
 			}
+
+			// changes width and height of the squares to fit in to the grid
 			$("li").css({"width": sideLength, "height": sideLength});
 			return generateMines();
 		}
 
-	// set mines
-
+		// gives chosen squares the class of 'mine'
 		function generateMines(){
 			mineNumbers = generateMineNumbers(gridWidth);
 
@@ -39,19 +45,14 @@ $(function() {
 				}
 			});
 
-			/*$("li").each(function(index, li){
-				console.log($(li).hasClass("mine"));
-			});*/
-
 		}
 
-	// randomly generate 2 mine locations (0-8)
-		// generate number of mines based on difficulty (0-(n-1))
-
+		// returns the values for the ids of the mine squares
 		function generateMineNumbers(){
 			var mineNumbers = []
 			var mines = 0;
 
+			// decides number of mines
 			if (gridWidth == 3) {
 				mines = 2;
 			} else if (gridWidth == 4) {
@@ -60,6 +61,7 @@ $(function() {
 				mines = gridWidth;
 			}
 
+			// chooses mine squares
 			for (var i = 0; i < mines; i++) {
 				mineNumbers.push(Math.floor(Math.random()*gridSize));
 			}
@@ -67,40 +69,42 @@ $(function() {
 			return mineNumbers;
 		}
 
-			// on click print a number
-
+		// runs when a square is clicked
 		function clickSquare(){
 			$("li").click(function() {
+				// if a mine is clicked on, prints * then runs 'gameOver'
 				if ($(this).hasClass("mine")) {
 					$(this).text("*");
 					gameOver();
 				} else {
+					// if not a mine, prints number of mines surrounding the square
 					var printNumber = getNumber($(this).attr("id"));
 					$(this).text(printNumber);
+
+					// removes the square from the array of non-mine squares
 					var id = parseInt($(this).attr("id"));
 					var blank = blankSquares.indexOf(id);
 					if (blank != -1) {
 						blankSquares.splice(blank, 1);
 					}
+
+					// checks if array of non-mine squares is empty, if true then game is won
 					checkEmpty(blankSquares);
 				}
 			});
 		}
 
-
-	// on click print a mine
-
-	// game over when mine clicked
+		// game over when mine clicked
 		function gameOver() {
 			console.log("GAME OVER");
 		}
 
-	// game win when all non-mines clicked
-	// APPEARING EARLY FIX THIS
+		// game win when all non-mines clicked
 		function gameWin(){
 			console.log("YOU WIN");
 		}
 
+		// checks if blankSquares array is empty, if it is runs gameWin
 		function checkEmpty() {
 			if (blankSquares.length == 0) {
 				gameWin();
@@ -109,15 +113,11 @@ $(function() {
 			}
 		}
 
-	// mine counter
-		// add logic for mine counter
+		// FUNCTIONS FOR CALCULATING NUMBER PRINTED IN SQUARE (MINECOUNT)
 
+		// splits squares into corners, sides and centres
 		function getNumber(square){
 			var mineCount = 0;
-			topArray = sideArray("a");
-			bottomArray = sideArray("b");
-			leftArray = sideArray("c");
-			rightArray = sideArray("d");
 
 			if ((square == 0) || (square == (gridWidth - 1)) || (square == ((Math.pow(gridWidth, 2) - gridWidth))) || (square == ((Math.pow(gridWidth, 2) - 1)))) {
 				mineCount = getCornerNumber(square);
@@ -130,196 +130,97 @@ $(function() {
 			return mineCount;
 		}
 
+		// calculate numbers for corners
 		function getCornerNumber(square) {
 			calcSquare = parseInt(square);
 			var mineCount = 0;
+
+				// top left corner
 				if (square == 0) {
-					// right
-					if (checkMine(calcSquare, "right") == true) {
-						mineCount++
-					} 
-				 	// below
-					if (checkMine(calcSquare, "below") == true) {
-						mineCount++
-					}
-					// lower right
-					if (checkMine(calcSquare, "down-right") == true) {
-						mineCount++
-					}
-				} else if (square == (gridWidth - 1)){
-					// left
-					if (checkMine(calcSquare, "left") == true) {
-						mineCount++
-					} 
-					// lower left
-					if (checkMine(calcSquare, "down-left") == true) {
-						mineCount++
-					}  
-					// below
-					if (checkMine(calcSquare, "below") == true) {
-						mineCount++
-					}
-				} else if (square == ((Math.pow(gridWidth, 2)) - gridWidth)) {
-					// above
-					if (checkMine(calcSquare, "above") == true) {
-						mineCount++
-					}
-					// upper right
-					if (checkMine(calcSquare, "up-right") == true) {
-						mineCount++
-					} 
-					// right
-					if (checkMine(calcSquare, "right") == true) {
-						mineCount++
-					}
-				} else if (square == ((Math.pow(gridWidth, 2) - 1))) {
-					// upper left
-					if (checkMine(calcSquare, "up-left") == true) {
-						mineCount++
-					} 
-					// above
-					if (checkMine(calcSquare, "above") == true) {
-						mineCount++
-					}
-					// left
-					if (checkMine(calcSquare, "left") == true) {
-						mineCount++
-					}
+					var locations = ["below", "right", "down-right"];
+					$(locations).each(function(index, location) {
+						if (checkMine(calcSquare, location)) mineCount++;
+					});
+				} 
+
+				// top right corner
+				else if (square == (gridWidth - 1)){
+					var locations = ["below", "left", "down-left"];
+					$(locations).each(function(index, location) {
+						if (checkMine(calcSquare, location)) mineCount++;
+					});
+				} 
+
+				// bottom left corner
+				else if (square == ((Math.pow(gridWidth, 2)) - gridWidth)) {
+					var locations = ["above", "right", "up-right"];
+					$(locations).each(function(index, location) {
+						if (checkMine(calcSquare, location)) mineCount++;
+					});
+				} 
+
+				// bottom right corner
+				else if (square == ((Math.pow(gridWidth, 2) - 1))) {
+					var locations = ["above", "left", "up-left"];
+					$(locations).each(function(index, location) {
+						if (checkMine(calcSquare, location)) mineCount++;
+					});
 				}
 
 			return mineCount;
 		}
 
+		// calculate numbers for sides
 		function getSideNumber(square, top, bottom, left, right){
 			mineCount = 0;
 			calcSquare = parseInt(square);
-			if (top.indexOf(calcSquare) != -1){
-				// left
-				if (checkMine(calcSquare, "left") == true) {
-					mineCount++
-				}
-				// right
-				if (checkMine(calcSquare, "right") == true) {
-					mineCount++
-				}
-				// lower left
-				if (checkMine(calcSquare, "down-left") == true) {
-					mineCount++
-				}  
-				// below
-				if (checkMine(calcSquare, "below") == true) {
-					mineCount++
-				}
-				// lower right
-				if (checkMine(calcSquare, "down-right") == true) {
-					mineCount++
-				}
 
-			} else if (bottom.indexOf(calcSquare) != -1){
-				// left
-				if (checkMine(calcSquare, "left") == true) {
-					mineCount++
-				}
-				// right
-				if (checkMine(calcSquare, "right") == true) {
-					mineCount++
-				} 
-				// upper right
-				if (checkMine(calcSquare, "up-right") == true) {
-					mineCount++
-				} 
-				// above
-				if (checkMine(calcSquare, "above") == true) {
-					mineCount++
-				}
-				// upper left
-				if (checkMine(calcSquare, "up-left") == true) {
-					mineCount++
-				}  
-			} else if (left.indexOf(calcSquare) != -1){
-				// right
-				if (checkMine(calcSquare, "right") == true) {
-					mineCount++
-				} 
-				// upper right
-				if (checkMine(calcSquare, "up-right") == true) {
-					mineCount++
-				} 
-				// above
-				if (checkMine(calcSquare, "above") == true) {
-					mineCount++
-				}
-				// below
-				if (checkMine(calcSquare, "below") == true) {
-					mineCount++
-				}
-				// lower right
-				if (checkMine(calcSquare, "down-right") == true) {
-					mineCount++
-				}
-			} else if (right.indexOf(calcSquare) != -1){
-				// left
-				if (checkMine(calcSquare, "left") == true) {
-					mineCount++
-				}
-				// above
-				if (checkMine(calcSquare, "above") == true) {
-					mineCount++
-				}
-				// upper left
-				if (checkMine(calcSquare, "up-left") == true) {
-					mineCount++
-				}  
-				// lower left
-				if (checkMine(calcSquare, "down-left") == true) {
-					mineCount++
-				} 
-				// below
-				if (checkMine(calcSquare, "below") == true) {
-					mineCount++
-				}
+			// top side
+			if (top.indexOf(calcSquare) != -1){
+				var locations = ["below", "left", "right", "down-left", "down-right"];
+				$(locations).each(function(index, location) {
+					if (checkMine(calcSquare, location)) mineCount++;
+				});
+			} 
+
+			// bottom side
+			else if (bottom.indexOf(calcSquare) != -1){
+				var locations = ["above", "left", "right", "up-left", "up-right"];
+				$(locations).each(function(index, location) {
+					if (checkMine(calcSquare, location)) mineCount++;
+				});
+			} 
+
+			// left side
+			else if (left.indexOf(calcSquare) != -1){
+				var locations = ["above", "below", "right", "up-right", "down-right"];
+				$(locations).each(function(index, location) {
+					if (checkMine(calcSquare, location)) mineCount++;
+				});
+			} 
+
+			// right side
+			else if (right.indexOf(calcSquare) != -1){
+				var locations = ["above", "below", "left", "up-left", "down-left"];
+				$(locations).each(function(index, location) {
+					if (checkMine(calcSquare, location)) mineCount++;
+				});
 			}
 			return mineCount;
 		}
 
+		// calculate numbers for centres
 		function getCenterNumber(square){
 			calcSquare = parseInt(square);
 			mineCount = 0;
-			// above
-			if (checkMine(calcSquare, "above") == true) {
-					mineCount++
-			}
-			// left
-			if (checkMine(calcSquare, "left") == true) {
-					mineCount++
-			}
-			// right
-			if (checkMine(calcSquare, "right") == true) {
-					mineCount++
-			}
-			// upper right
-			if (checkMine(calcSquare, "up-right") == true) {
-					mineCount++
-			} 
-			// upper left
-			if (checkMine(calcSquare, "up-left") == true) {
-					mineCount++
-			}  
-			// lower left
-			if (checkMine(calcSquare, "down-left") == true) {
-					mineCount++
-			} 
-			// below
-			if (checkMine(calcSquare, "below") == true) {
-					mineCount++
-			}
-			// lower right
-			if (checkMine(calcSquare, "down-right") == true) {
-					mineCount++
-			}
+			var locations = ["above", "below", "left", "right", "up-left", "up-right", "down-left", "down-right"];
+			$(locations).each(function(index, location) {
+				if (checkMine(calcSquare, location)) mineCount++;
+			});
 			return mineCount;
 		}
 
+		// calculations that check for mines
 		function checkMine(square, position) {
 			if (position == "above"){
 				if ($("#" + (calcSquare - gridWidth)).hasClass("mine") == true){
@@ -354,23 +255,22 @@ $(function() {
 					return true;
 				}
 			}
-
 		}
 
+		// generates arrays of squares on the edge of the grid
 		function sideArray(side) {
 			var sideArray = [];
 			for (i = 1; i < (gridWidth - 1); i++) {
-				if (side == "a") {
+				if (side == "top") {
 					sideArray.push(i);
-				} else if (side == "b") {
+				} else if (side == "bottom") {
 					sideArray.push((gridSize - 1) - i);
-				} else if (side == "c") {
+				} else if (side == "left") {
 					sideArray.push(i*gridWidth);
-				} else if (side == "d") {
+				} else if (side == "right") {
 					sideArray.push((i*gridWidth)+(gridWidth - 1));
 				}
 			}
-				
 			return sideArray;
 		}
 
